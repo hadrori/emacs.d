62428aeda9987c86d4b8b5256cc3cea77592967b
hadrori <hadrori.hs@gmail.com>
2016-01-18 01:53:54 +0900

    Fix init.el comment

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; load ~/.emacs.d/site-list/*
+;; load ~/.emacs.d/site-list/
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
   (add-to-list 'load-path default-directory)
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
