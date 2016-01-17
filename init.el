9b5581f4430bef90f3ebc5391f4c2d5488c18199
hadrori <hadrori.hs@gmail.com>
2016-01-18 01:41:19 +0900

    Add init loader and basic settings

---
+++ init.el
@@ -0,0 +1,10 @@
+;; load ~/.emacs.d/site-list/*
+(let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
+;; init-loader
+(require 'init-loader)
+(setq init-loader-show-log-after-init 'error-only)
+(init-loader-load "~/.emacs.d/inits")
---
+++ inits/00-keybinds.el
@@ -0,0 +1,3 @@
+(keyboard-translate ?\C-h ?\C-?)
+(global-set-key "\M-g" 'goto-line)
+
