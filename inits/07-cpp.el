e23ec6b587525f33ee396d253c101672e3b84530
hadrori <hadrori.hs@gmail.com>
2016-01-23 17:47:40 +0900

    Update c++-mode

--- inits/07-cpp.el
+++ inits/07-cpp.el
@@ -1,2 +1,8 @@
-(setq c-default-style "linux"
-      c-basic-offset 4)
+(defun hadrori-c-mode-hook ()
+    (setq c-default-style "linux"
+          c-basic-offset 4
+          c-hungry-delete-key t)
+    (c-set-offset 'innamespace 0))
+
+(add-hook 'c++-mode-hook 'hadrori-c-mode-hook)
+(add-hook 'c-mode-hook 'hadrori-c-mode-hook)
