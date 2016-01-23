77bb835873a8b94b1be79d1428b9f4c8f635d2e2
hadrori <hadrori.hs@gmail.com>
2016-01-23 17:50:12 +0900

    Update ruby el

--- inits/08-ruby.el
+++ inits/08-ruby.el
@@ -1,2 +1,4 @@
-(setq ruby-insert-encoding-magic-comment nil
-      ruby-indent-level 2)
+(defun hadrori-ruby-mode-hook ()
+  (setq ruby-insert-encoding-magic-comment nil
+        ruby-indent-level 2))
+(add-hook 'ruby-mode-hook 'hadrori-ruby-mode-hook)
