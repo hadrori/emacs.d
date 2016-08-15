00f2be3c8cad4f58651ca0a064e37b15fc3550df
hadrori <hadrori.hs@gmail.com>
2016-08-15 16:40:28 +0900

    Add Cask

--- inits/09-yasnipeet.el
+++ inits/09-yasnipeet.el
@@ -1,6 +1,3 @@
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/elpa/yasnippet-20140617.1640"))
-(require 'yasnippet)
 (setq yas-sinippet-dirs
       '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
