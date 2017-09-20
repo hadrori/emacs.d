553311f426bf2a3ad50d647fbaca94da1e047f48
kisozuka <kisozuka@indeed.com>
2017-09-20 13:03:49 -0500

    Merge linux windows

--- init.el
+++ init.el
@@ -1,13 +1,14 @@
-;; cask
-
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(require 'cask)
+;; cask
+(when (eq system-type 'darwin)
+  (require 'cask))
+(when (or (eq system-type 'gnu/linux) (eq system-type 'windows-nt))
+  (require 'cask "~/.cask/cask.el"))
 (cask-initialize)
 
 ;; use-package
